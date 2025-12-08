1a2fba729ca7cb11e8cfcd687f4677cd232b1b19
Ričards Brauns <ricardsbraunins@gmail.com>
2025-12-08 18:26:54 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const supabase = createClient(
-  'YOUR_SUPABASE_URL',
-  'YOUR_SUPABASE_ANON_KEY'
+  'https://hauxhvjdigskrobelamj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhdXhodmpkaWdza3JvYmVsYW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxOTc4NDIsImV4cCI6MjA4MDc3Mzg0Mn0.67DccJcyS5nGqcmn5ooLGBsB01E3CfwS8tPHv8djvOU'
 );
